2074d6eabeda80b35de18b439828f3582049d370
amutake <amutake.s@gmail.com>
2013-12-03 20:11:16 +0900

    Add a test case

---
+++ test/test.hs
@@ -0,0 +1,96 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Applicative
+import qualified Data.ByteString.Lazy as L
+import Data.Conduit
+import Data.Text (Text)
+import Test.Hspec
+import Text.XML.Stream.Parse
+
+import Cloud.AWS.Lib.Parser.Unordered
+
+main :: IO ()
+main = hspec $ do
+    describe "xml parser" $ do
+        it "parse normal xml" parseNormal
+
+data TestData = TestData
+    { testDataId :: Int
+    , testDataName :: Text
+    , testDataDescription :: Maybe Text
+    , testDataItemsSet :: [TestItem]
+    } deriving (Eq, Show)
+
+data TestItem = TestItem
+    { testItemId :: Int
+    , testItemName :: Text
+    , testItemDescription :: Maybe Text
+    , testItemSubItem :: Maybe TestItem
+    } deriving (Eq, Show)
+
+parseTestData :: (MonadThrow m, Applicative m) => SimpleXML -> m TestData
+parseTestData xml = TestData
+    <$> getT xml "id"
+    <*> getT xml "name"
+    <*> getT xml "description"
+    <*> getElements xml "itemSet" "item" parseTestItem
+
+parseTestItem :: (MonadThrow m, Applicative m) => SimpleXML -> m TestItem
+parseTestItem xml = TestItem
+    <$> getT xml "id"
+    <*> getT xml "name"
+    <*> getT xml "description"
+    <*> getElementM xml "subItem" parseTestItem
+
+parseNormal :: Expectation
+parseNormal = do
+    d <- runResourceT $ parseLBS def input $$ xmlParser (\xml -> getElement xml "data" parseTestData)
+    d `shouldBe` input'
+  where
+    input = L.concat
+        [ "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+        , "<data>"
+        , "  <id>1</id>"
+        , "  <name>test</name>"
+        , "  <description>this is test</description>"
+        , "  <itemSet>"
+        , "    <item>"
+        , "      <id>1</id>"
+        , "      <name>item1</name>"
+        , "      <description>this is item1</description>"
+        , "      <subItem>"
+        , "        <id>11</id>"
+        , "        <name>item1sub</name>"
+        , "      </subItem>"
+        , "    </item>"
+        , "    <item>"
+        , "      <id>2</id>"
+        , "      <name>item2</name>"
+        , "    </item>"
+        , "  </itemSet>"
+        , "</data>"
+        ]
+    input' = TestData
+        { testDataId = 1
+        , testDataName = "test"
+        , testDataDescription = Just "this is test"
+        , testDataItemsSet =
+            [ TestItem
+                { testItemId = 1
+                , testItemName = "item1"
+                , testItemDescription = Just "this is item1"
+                , testItemSubItem = Just TestItem
+                    { testItemId = 11
+                    , testItemName = "item1sub"
+                    , testItemDescription = Nothing
+                    , testItemSubItem = Nothing
+                    }
+                }
+            , TestItem
+                { testItemId = 2
+                , testItemName = "item2"
+                , testItemDescription = Nothing
+                , testItemSubItem = Nothing
+                }
+            ]
+        }
